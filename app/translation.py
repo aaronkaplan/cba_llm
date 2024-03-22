3118878e57da4cec0c8b203641e7acb6e9e4d6c2
aaronkaplan <aaron@lo-res.org>
2024-03-22 19:38:45 +0100

    stub translation module

---
+++ app/translation.py
@@ -0,0 +1,16 @@
+"""Simple module to translate a string in a language to the reference language (english)"""
+
+
+def translate(text: str, dest: str = 'en') -> str:
+    """Translate a string to the reference language (english)
+
+    Arguments:
+    text -- the string to translate
+    dest -- the destination language (default 'en')
+
+    Returns:
+    str -- the translated string
+    """
+    return "foo"
+
+
