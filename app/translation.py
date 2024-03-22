1440f650046ee526b077e7c6e6e5be9357b3dd0a
aaronkaplan <aaron@lo-res.org>
2024-03-22 21:40:19 +0100

    original_text -> src_text. output_text -> dst_text

--- app/translation.py
+++ app/translation.py
@@ -9,21 +9,22 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
 
 class Translation(BaseModel):
-    original_text: str =  Field(description="The original text to translate")
-    translated_text: str = Field(description="The translated text")
-    # original_language: str = Field(description="The original language of the text")
-    translated_language:  str = Field(description="The language the text was translated to")
+    """Pydantic class to represent a translation result."""
+    src_text: str =  Field(description="The original text to translate")
+    dst_text: str = Field(description="The translated text")
+    src_language: str = Field(description="The original language of the text", required=False)
+    dst_language:  str = Field(description="The language the text was translated to")
 
     def __init__(self, data):
         super().__init__(**data)
 
 
-def translate(original_text: str, output_language: str = 'english') -> str:
+def translate(src_text: str, dst_language: str = 'english') -> str:
     """Translate a string to the reference language (english)
 
     Arguments:
-    text -- the string to translate
-    output_language -- the destination language (default 'english')
+    src_text -- the string to translate
+    dst_language -- the destination language (default 'english')
 
     Returns:
     str -- the translated string
@@ -47,10 +48,11 @@ def translate(original_text: str, output_language: str = 'english') -> str:
     chain = prompt | model | output_parser
 
     # now use the llm with the prompt to translate the text
-    result = chain.invoke({"text": original_text, "output_language": output_language})
+    result = chain.invoke({"src_text": src_text, "dst_language": dst_language})
 
-    translation = Translation({'original_text': original_text,
-                               'translated_text': result['translated_text'], 
-                               'translated_language': output_language})
-    return translation.translated_text
+    translation = Translation({'src_text': src_text,
+                               'dst_text': result['translated_text'], 
+                               'src_language': result['src_language'],
+                               'dst_language': dst_language})
+    return translation.dst_text
 
--- tests/test_translation.py
+++ tests/test_translation.py
@@ -5,7 +5,7 @@ from app.translation import translate
 def test_translate():
     """Test the translate function."""
     text = "Esto es una prueba"
-    result = translate(text, output_language="english")
+    result = translate(text, dst_language="english")
     assert result == "This is a test"
-    result = translate(text, output_language="deutsch")
+    result = translate(text, dst_language="deutsch")
     assert result != "This is a test"
